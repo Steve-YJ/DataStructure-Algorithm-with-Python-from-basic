30186029378df2483d4ab1c7d0b512a89855fd60
HesseyInsight <leeyoungjeon0511@gmail.com>
2020-04-13 18:44:02 +0900

    :bookmark: confused..2

--- 02. 게임으로 익히는 코딩 알고리즘/by_Myself/Ch15.RollerCoaster_Try1.py
+++ 02. 게임으로 익히는 코딩 알고리즘/by_Myself/Ch15.RollerCoaster_Try1.py
@@ -16,33 +16,37 @@ print('input queue: ', queue)
 
 # sum
 # sum = 0
+
+# total은 총운행수입
+# sum은 운행당 수입의 합계
 total = 0
-for i in range(c):
+for i in range(c):  # c번 운행하는 롤러코스터
     print('tern', i)
     sum = 0
     print("initialization sum: ", sum)
     count = 0
-    Tqueue = queue
-    for i in range(len(Tqueue)):  # loop four times
+    Tqueue = []
+    for i in range(len(queue)):  # loop four times
         # while sum >= l: 
-        if (sum + Tqueue[i]) < l:
-            sum += Tqueue[i]
+        if (sum + queue[i]) < l:
+            sum += queue[i]
             print('print sum: ', sum)
             count += 1    
             print('print_count: ', count)
             total += sum
-            Tqueue = Tqueue[count:] + Tqueue[:count]
+            Tqueue = queue[count:] + queue[:count]
             print("Tqueue: ", Tqueue)
 
-        elif (sum + Tqueue[i]) == l:
-            sum += Tqueue[i]
+        elif (sum + queue[i]) == l:
+            sum += queue[i]
             total += sum 
             count +=1
             for i in range(count):
-                Tqueue = Tqueue[count:] + Tqueue[:count]
+                queue = queue[count:] + queue[:count]
         else:
-            break
-    print(Tqueue)
+            # queue = Tqueue
+            pass
+    print(sum)
 print("answer")
 print("total: ", total)
-print("Tqueue: ", Tqueue)
+print("queue: ", Tqueue)
